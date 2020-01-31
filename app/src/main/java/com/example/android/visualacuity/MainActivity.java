3c05f1765310bb06ac364a9b40cf2d97cbf4a7c9
helixjolt <siddharth.byale17@siesgst.ac.in>
2020-01-31 17:49:02 +0530

    Stable swipe & rotation code

---
+++ app/src/main/java/com/example/android/visualacuity/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.android.visualacuity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+import com.github.pwittchen.swipe.library.rx2.Swipe;
+import com.github.pwittchen.swipe.library.rx2.SwipeListener;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Swipe swipe;
+    public int r;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //Max Brightness
+        WindowManager.LayoutParams layout = getWindow().getAttributes();
+        layout.screenBrightness = 1F;
+        getWindow().setAttributes(layout);//End
+
+        final ViewFlipper viewFlipper = findViewById(R.id.ViewFlipper);
+
+        swipe = new Swipe();
+        swipe.setListener(new SwipeListener() {
+            @Override public void onSwipingLeft(final MotionEvent event) {
+                //info.setText("SWIPING_LEFT");
+                //viewFlipper.showPrevious();
+            }
+
+            @Override public boolean onSwipedLeft(final MotionEvent event) {
+                //info.setText("SWIPED_LEFT");
+                //viewFlipper.showNext();
+                rotation();
+                return true;
+            }
+
+            @Override public void onSwipingRight(final MotionEvent event) {
+                //info.setText("SWIPING_RIGHT");
+                //viewFlipper.showNext();
+            }
+
+            @Override public boolean onSwipedRight(final MotionEvent event) {
+                //info.setText("SWIPED_RIGHT");
+                viewFlipper.showNext();
+                return true;
+
+            }
+
+            @Override public void onSwipingUp(final MotionEvent event) {
+                //info.setText("SWIPING_UP");
+            }
+
+            @Override public boolean onSwipedUp(final MotionEvent event) {
+                //info.setText("SWIPED_UP");
+                viewFlipper.showNext();
+                return true;
+            }
+
+            @Override public void onSwipingDown(final MotionEvent event) {
+                //info.setText("SWIPING_DOWN");
+            }
+
+            @Override public boolean onSwipedDown(final MotionEvent event) {
+                //info.setText("SWIPED_DOWN");
+                viewFlipper.showPrevious();
+                return true;
+            }
+        });
+
+    }
+
+    public void rotation(){
+        Random rand = new Random();
+        r=rand.nextInt(4);
+        ImageView imgview;
+        imgview = (ImageView)findViewById(R.id.imageView1);
+        imgview.setRotation((float) 90.0*r);
+    }
+
+
+    @Override public boolean dispatchTouchEvent(MotionEvent event) {
+        swipe.dispatchTouchEvent(event);
+        return super.dispatchTouchEvent(event);
+    }
+
+}
