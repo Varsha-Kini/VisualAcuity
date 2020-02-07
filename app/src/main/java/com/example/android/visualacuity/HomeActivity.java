12f1ce8586ad5ccb3712e6d4a7888071a41af00a
helixjolt <siddharth.byale17@siesgst.ac.in>
2020-02-07 13:09:49 +0530

    e img update

--- app/src/main/java/com/example/android/visualacuity/HomeActivity.java
+++ app/src/main/java/com/example/android/visualacuity/HomeActivity.java
@@ -30,7 +30,7 @@ public class HomeActivity extends AppCompatActivity {
     public double[] dividend = new double[]{60, 48, 38, 30, 24, 19, 15, 12, 9.5, 7.5, 6};
     public int[] feetDividend = new int[] {200, 160, 125, 100, 80, 63, 50, 40, 32, 25, 20};
     public double[] logMARList = new double[]{1.00, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0};
-    public int[] imageList = new int[]{R.drawable.e_0, R.drawable.e_1, R.drawable.e_2,
+    public int[] imageList = new int[]{R.drawable.ic_e_29, R.drawable.e_1, R.drawable.e_2,
                         R.drawable.e_3, R.drawable.e_4, R.drawable.e_5, R.drawable.e_6,
                         R.drawable.e_7, R.drawable.e_8, R.drawable.e_9, R.drawable.e_10};    //11 images added to list
 
