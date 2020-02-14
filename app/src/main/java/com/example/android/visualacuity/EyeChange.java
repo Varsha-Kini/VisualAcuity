23de10853cc2cd5813b3504a03376df27311d071
helixjolt <siddharth.byale17@siesgst.ac.in>
2020-02-14 19:38:47 +0530

    Color scheme added + minor changes

--- app/src/main/java/com/example/android/visualacuity/EyeChange.java
+++ app/src/main/java/com/example/android/visualacuity/EyeChange.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class EyeChange extends AppCompatActivity {
 
@@ -24,5 +25,11 @@ public class EyeChange extends AppCompatActivity {
                 EyeChange.this.finish();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //do nothing
+        Toast.makeText(getBaseContext(), "Error: Test not completed",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/android/visualacuity/HomeActivity.java
+++ app/src/main/java/com/example/android/visualacuity/HomeActivity.java
@@ -52,6 +52,7 @@ public class HomeActivity extends AppCompatActivity {
                                         |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                                         |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
+
         //For vibration on swipe
         final Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
@@ -179,7 +180,8 @@ public class HomeActivity extends AppCompatActivity {
                 logMAR[0] = logMARList[i] + (0.02 * flag);
                 logMAR[0] = Math.round(logMAR[0] * 10000d) / 10000d;
                 Intent myIntent1=new Intent(HomeActivity.this,EyeChange.class);
-                startActivity(myIntent1);
+                HomeActivity.this.startActivity(myIntent1);
+                HomeActivity.this.finish();
             }
             else if(iteration==1){
                 s6[1] = "6/" + (int) dividend[i] + " - " + flag;
@@ -195,17 +197,40 @@ public class HomeActivity extends AppCompatActivity {
                 myIntent.putExtra("Lsnellen6", s6[1]);
                 myIntent.putExtra("Lsnellen20", s20[1]);
                 myIntent.putExtra("LlogMAR", logMAR[1]);
-                startActivity(myIntent);
+                HomeActivity.this.startActivity(myIntent);
+                HomeActivity.this.finish();
             }
 
         }
     }
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = this.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    @Override
     public void onBackPressed() {
-        //Go back to home screen
-        Intent mainIntent = new Intent(HomeActivity.this, MainActivity.class);
-        HomeActivity.this.startActivity(mainIntent);
-        HomeActivity.this.finish();
+    // do nothing
     }
 }
